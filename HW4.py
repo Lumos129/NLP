1e1bbcab8355d7ca5d265fd3a53afc157b4f9379
Lumos129 <gonglf9@gmail.com>
2021-08-16 04:26:21 +0800

    Init HW4.py

--- HW4.py
+++ HW4.py
@@ -4,11 +4,6 @@ import json
 
 import word2vec     # v0.9.4
 
-"""
-1. 将opencc文件夹复制到D盘根目录
-2. 注意, 在类的__init__中, 正确设置wiki_zh_2019文件夹的路径
-"""
-
 
 class HW4(object):
     def __init__(self, corpus_path: object = 'D:\我的資料\wiki_zh_2019') -> object:
@@ -54,7 +49,7 @@ class HW4(object):
 
         print('jieba cutting.....')
 
-        jieba.setLogLevel(jieba.logging.INFO)   # 引入jieba模块后加入该代码，代码即可不报警
+        jieba.setLogLevel(jieba.logging.INFO)
         jieba.set_dictionary('dict.txt')
         idx=0
         with open(self.corpus_cut, 'w', encoding='utf-8') as fw:
@@ -70,16 +65,7 @@ class HW4(object):
                     if idx % 1000 == 0:
                         print(len(lines_rd)-idx)
 
-                """
-                while True:
-                    line_rd = fr.readline()
-                    if not line_rd:
-                        break
-                    terms_list = jieba.lcut(line_rd, cut_all=False, HMM=True)
-                    terms_list = [term for term in terms_list if term not in self.stop_words]
-                    line_wr = ' '.join(terms_list) + ' '
-                    fw.write(line_wr)
-                """
+               
 
     def train(self):
         word2vec.word2vec(self.corpus_cut, self.model_file, size=100, verbose=True, window=5)
